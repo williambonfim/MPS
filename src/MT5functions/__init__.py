131fa59b9625c13533e43855fafe9e1f90ec25bf
williambonfim <119766696+williambonfim@users.noreply.github.com>
2023-01-08 23:07:25 +0000

    Update __init__.py

--- src/MT5functions/__init__.py
+++ src/MT5functions/__init__.py
@@ -1,6 +1,1 @@
 from .functions import MT5
-from .strategies import StrategiesFunctions
-from .strategies import Strategy_at_time
-from .strategies import Strategy_at_time_sl_tp
-from .strategies import Strategy_at_sl
-from .strategies import Strategy_at_opening
